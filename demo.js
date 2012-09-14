9333ba5ba4174e40447fab4d8f5565286452b3db
xinwei.wang <wxwdesign@gmail.com>
2012-09-14 18:13:37 +0800

    updat demo

--- demo.js
+++ demo.js
@@ -1,39 +1,41 @@
-var dd=document.getElementsByTagName('dd');
+var dl=document.getElementsByTagName('dl'),
+	dd1=dl[0].getElementsByTagName("dd"),
+	dd2=dl[1].getElementsByTagName("dd");
 
-/* for(var i=0,L=dd.length;i<L;i++){
-	(function(c){
-		var t=1,
-			timer=new TimerCenter(c*1000).start(),
-			s=false;
-		
-		timer.addListener('dd',function(){
-			t+=c;
-			dd[c-1].innerHTML=t;
-		});
-		
-		dd[c-1].onclick=function(){
-			s?timer.resumeListeners('dd'):timer.stopListeners('dd');
-			s=!s;
-		}
-	})(i+1);
-} */
-
-var timer=new TimerCenter().start().start().start();
-for(var i=0,L=dd.length;i<L;i++){
+var timer=new TimerCenter(500).start().start().start();
+for(var i=0,L=dd1.length;i<L;i++){
 	(function(c){
 		var t=1,
 			s=false;
 		
 		//add timer listeners
-		timer.addListener('dd',function(){
+		timer.addListener('dd1',function(){
 			t+=c;
-			dd[c-1].innerHTML=t;
+			dd1[c-1].innerHTML=t;
 		});
 		
 		//click event stop or resume
-		dd[c-1].onclick=function(){
-			// s?timer.resumeListeners('dd'):timer.stopListeners('dd');
+		dd1[c-1].onclick=function(){
+			// s?timer.resumeListeners('dd1'):timer.stopListeners('dd1');
 			s?timer.resume():timer.stop();
+			s=!s;
+		}
+	})(i+1);
+}
+
+for(var i=0,L=dd2.length;i<L;i++){
+	(function(c){
+		var t=1,
+			timer=new TimerCenter(c*1000).start(),
+			s=false;
+		
+		timer.addListener('dd2',function(){
+			t+=c;
+			dd2[c-1].innerHTML=t;
+		});
+		
+		dd2[c-1].onclick=function(){
+			s?timer.resumeListeners('dd2'):timer.stopListeners('dd2');
 			s=!s;
 		}
 	})(i+1);
